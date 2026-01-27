d386a8af2f797939033ae0f3a00a7b8853e34950
pcmwangi <pcmwangi@co-opbank.co.ke>
2026-01-27 18:27:19 +0300

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,13 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serializer)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 android {
@@ -10,6 +16,10 @@ android {
         version = release(36) {
             minorApiLevel = 1
         }
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
 
     defaultConfig {
@@ -32,15 +42,25 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.fromTarget("17")
+
+    }
+}
+
+
+composeCompiler {
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
 }
 
 dependencies {
@@ -59,4 +79,33 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.ktorSerializer)
+    implementation(libs.ktorClientCore)
+    implementation(libs.ktorClientAndroid)
+    implementation(libs.ktorClientAuth)
+    implementation(libs.ktorNegotiation)
+    implementation(libs.ktorLogging)
+    implementation(libs.ktorOkhttp)
+    implementation(libs.ktorNetwork)
+    implementation(libs.ktorNetworkTls)
+
+
+    implementation(libs.timber)
+
+
+    implementation(libs.room.compiler)
+    implementation(libs.room.ktx)
+    implementation(libs.room.runtime)
+
+    debugImplementation(libs.chucker.debug)
+    releaseImplementation(libs.chucker.release)
+
+
+    ksp(libs.hilt.android)
+    implementation(libs.hilt.android.compiler)
+
+
+
+
 }
